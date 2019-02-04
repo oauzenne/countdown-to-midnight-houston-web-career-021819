2a8b4e303b5835dc093b06c48733ae2e3240bdaf
Olivia Auzenne <oauzenne@gmail.com>
2019-02-04 18:04:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,4 +6,11 @@ def countdown(number)
 return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(1.second)
+def countdown_with_sleep(number)
+  while number>0
+    sleep(1)
+    puts "#{number} SECOND(S)!"
+  number-=1
+  end
+return "HAPPY NEW YEAR!"
+end
